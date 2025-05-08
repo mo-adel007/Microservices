9a941aba243fa2e975556cbb96c1a71eaa871635
mo-adel007 <muhamedadelfahmy99@gmail.com>
2025-05-08 13:50:15 +0300

    inital commit

---
+++ identity-service/src/server.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const logger = require("./utils/logger.js");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const app = express();
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const routes = require("./routes/identity-service.js");
+const errorHandler = require("./middleware/errorHandler.js");
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    logger.info("MongoDB connected!");
+  })
+  .catch((err) => {
+    logger.error("MongoDB connection failed:", err);
+  });
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use((req, res, next) => {
+  logger.info(`Recieved ${req.method} to ${req.url}`);
+  logger.info(`Request body: ${req.body}`);
+  next();
+});
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
+      res.status(429).send({ success: false, message: "Too Many Requests" });
+    });
+});
+
+const sensitveEndpointsLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (req, res, next) => {
+    logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).send({ success: false, message: "Too Many Requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use("/api/auth/register", sensitveEndpointsLimiter);
+app.use("/api/auth", routes);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  logger.info(`Identity service Server is running on port ${process.env.PORT}`);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error({ "Unhandled Rejection:": promise, reason: reasom });
+});
---
+++ identity-service/src/utils/logger.js
@@ -0,0 +1,43 @@
+const winston = require("winston");
+
+// Create a logger instance
+const logger = winston.createLogger({
+  // Set logging level based on environment
+  // In production, use 'info' (less verbose); otherwise use 'debug'
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+
+  // Define how logs should be formatted
+  format: winston.format.combine(
+    winston.format.colorize(), // Add color to log levels (useful in console)
+    winston.format.timestamp(), // Include timestamp in logs
+    winston.format.errors({ stack: true }), // Capture stack trace if available
+    winston.format.splat(), // Support for printf-style log messages
+    winston.format.json(), // Format logs as JSON (useful for files/monitoring)
+
+    // Custom format to control final log structure
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+
+  // Add default metadata to each log entry
+  defaultMeta: { service: "identity-service" },
+
+  // Configure output destinations (transports)
+  transports: [
+    // Console transport with basic formatting for readability during development
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(), // Keep colorization for console
+        winston.format.timestamp(), // Include timestamp
+        winston.format.simple() // Simpler output format for dev readability
+      ),
+    }),
+
+    // File transport for saving logs persistently
+    new winston.transports.File({ filename: "error.log", level: "error" }), // Save all logs
+
+    new winston.transports.File({ filename: "combine.log" }),
+  ],
+});
+module.exports = logger;
